150dc84171f21a6b98dadafcb6eeba44e4835a8c
Tiffany Kyu <tiffanykyu@tiffanys-macbook-pro-3.local>
2017-03-08 12:45:12 -0800

    Initial Commit

---
+++ maps/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  maps
+//
+//  Created by Tiffany Kyu on 3/8/17.
+//  Copyright © 2017 Tiffany Kyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
